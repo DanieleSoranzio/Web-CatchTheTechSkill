ce496a342744e026f72ab8db34e094e84966c831
DanieleSoranzio <122631270+DanieleSoranzio@users.noreply.github.com>
2026-04-10 15:30:06 +0200

    Init Project, Text Build Webgl

---
+++ CatchTheTechSkill/Assets/Scripts/PlayerBevahiour.cs
@@ -0,0 +1,38 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerBevahiour : MonoBehaviour
+{
+    #region Variables
+    Rigidbody2D rb;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private int lives=3;
+    private float horizontal;
+    
+    #endregion
+   
+    #region Mono
+    void Awake()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        rb.linearVelocity=new Vector2(horizontal*movementSpeed*Time.deltaTime,0);  
+    }
+    
+    #endregion
+    
+    #region Player_Controller
+
+    public void MoveAction(InputAction.CallbackContext ctx)
+    {
+        horizontal=ctx.ReadValue<Vector2>().x;
+    }
+    
+    #endregion
+}
